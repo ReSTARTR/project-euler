42d7ef7f3653e34876af2ef285bd290b19feb2db
ReSTARTR <yoshida.masaki+restartr@gmail.com>
2013-04-18 20:32:04 +0900

    ruby: Solved 003

---
+++ ruby/003.rb
@@ -0,0 +1,50 @@
+def prime_factors(x)
+    Enumerator.new do |g|
+        table = []
+        (2...x).each do |i|
+            if table.include?(i)
+                g.yield i
+            elsif x % i == 0
+                g.yield i
+                table.push(i)
+                x = x / i
+            end
+        end
+    end
+end
+
+
+def primes(x)
+    nums = []
+    prime_factors(x).each do |y|
+        if y > 0
+            nums.push(y)
+        else
+            break
+        end
+    end
+    nums
+end
+
+
+def solve(x)
+    return primes(x).max
+end
+
+
+require 'test/unit'
+class TestSolve < Test::Unit::TestCase
+    def test_prime_factors
+        assert prime_factors(10).to_a == [2, 5]
+        assert prime_factors(13195).to_a == [5, 7, 13, 29]
+    end
+
+    def test_primes
+        assert primes(10) == [2, 5]
+        assert primes(13195) == [5, 7, 13, 29]
+    end
+
+    def test_solve
+        #assert solve(600851475143) == 6857
+    end
+end
